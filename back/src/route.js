03742c2818298de52c0be21143e19e7736427c95
luisnarv <115510650+luisnarv@users.noreply.github.com>
2024-03-12 13:28:32 -0500

    guardar el archivo al usuario final

--- back/src/route.js
+++ back/src/route.js
@@ -7,36 +7,26 @@ const fs = require("fs");
 
 const router = Router();
 
+//Enviar el video al front end
 router.use("/download/video", async (req, res) => {
   try {
+    //Obtenemos la URL del video por query
     const videoId = req.query.url;
-
-    const outputName = "video.mp4";
-    const downloadDirectory = "G:/Descargas";
-    const outputPath = path.resolve(`${downloadDirectory}/`, outputName);
-
-    const format = ytdl(videoId);
-
-    format.on("info", (info) => {
-      console.log("title:", info.videoDetails.title);
-      console.log("rating:", info.player_response.videoDetails.averageRating);
-      console.log("uploaded by:", info.videoDetails.author.name);
+    //Buscamos la información del video y filtramos por audio y video
+    const info = await ytdl.getInfo(videoId);
+    // const format = ytdl.filterFormats(info.formats, "videoandaudio");
+    const format = ytdl.chooseFormat(info.formats, {
+      filter: (format) => format.container === "videoandaudio",
     });
 
-    format.on("progress", (chunkLength, downloaded, total) => {
-      const percent = downloaded / total;
-      console.log("downloading", `${(percent * 100).toFixed(1)}%`);
-    });
-
-    format.on("end", () => {
-      console.log("saved to", outputName);
-    });
-
-    format.pipe(fs.createWriteStream(outputPath));
+    res.set("Content-Type", "video/mp4");
+    // format.pipe(res);
+    console.log(format);
   } catch (error) {
     console.log(error.message);
   }
 });
+
 // router.use("/download/video", async (req, res) => {
 //   try {
 //     const videoId = req.query.url;
@@ -57,17 +47,27 @@ router.use("/download/video", async (req, res) => {
 //   }
 // });
 
+//formato devuelto es ,audio/ mp4 y webbm cambiar el formato antes de anviar al front
 router.use("/download/audio", async (req, res) => {
   try {
-    console.log(req.query.url);
     const videoId = req.query.url;
     const info = await ytdl.getInfo(videoId);
-    const format = ytdl.filterFormats(info.formats, "audioonly", {
-      quality: "tiny",
-    });
+    const format = ytdl.filterFormats(info.formats, "audioonly");
 
-    const videoUrl = format[0].url;
-    res.redirect(videoUrl);
+    res.set("Content-Type", "audio/mp3");
+    // Establecer el encabezado Content-Disposition para indicar el nombre del archivo
+    res.set("Content-Disposition", `attachment; filename=${info.title}.mp3`);
+
+    let videoUrl;
+    let index = 0;
+    for (let url of format) {
+      if (url.mimeType.includes("mp4")) {
+        videoUrl = format[index].url;
+      }
+      index++;
+    }
+    // const videoUrl = format[0].url;
+    res.send(videoUrl);
   } catch (error) {
     console.log(error.message);
   }
